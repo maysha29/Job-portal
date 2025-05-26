a9b5eb67126767464760e66d4537a207ec3eb2f1
maysha29 <mayshatabassum29@gmail.com>
2025-05-26 18:39:16 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/router/router.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home";
+import Register from "../Pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children:[
+      {
+        index:true,
+        Component:Home,
+      }, 
+      {
+        path:'/register',
+        Component:Register,
+      }
+    ]
+  },
+]);
+
+export default router;
