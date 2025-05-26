cf68eb7cf029924718e56e426ad546cc978be7a8
maysha29 <mayshatabassum29@gmail.com>
2025-05-26 19:32:13 +0600

    register page is added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
+import Lottie from "lottie-react";
+import registerLottie from '../assets/lotties/register.json';
 
 const Register = () => {
+     const handleRegister=e=>{
+        e.preventDefault();
+        const form=e.target;
+        const email=form.email.value;
+        const password=form.password.value;
+        console.log({email,password});
+
+     }
+
+
     return (
         <div>
            <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
+        <Lottie animationData={registerLottie} loop={true}></Lottie>
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
+        <form onSubmit={handleRegister} >
         <fieldset className="fieldset">
             <h1 className="text-5xl font-bold">Register now!</h1>
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" name='email' placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" className="input" name='password' placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
+        </form>
       </div>
     </div>
   </div>
